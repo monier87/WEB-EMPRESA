9aee8c2f8c741dac00ec1824ad8620c55fb8a205
Carlos David <carlos.monier87@gmail.com>
2022-12-13 09:27:59 -0600

    Prueba 5

--- services/views.py
+++ services/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Service
 
 # Create your views here.
 
 def servicios(request):
-    return render(request, 'services/servicios.html')
+    services = Service.objects.all()
+    return render(request, 'services/servicios.html', {'services': services})
