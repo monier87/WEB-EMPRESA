1e0d691d71e8917055ead93d05d6e965eba46816
Carlos David <carlos.monier87@gmail.com>
2022-12-19 22:44:34 -0600

    Add app Pages1

--- pages/models.py
+++ pages/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Page(models.Model):
     title= models.CharField(verbose_name="Titulo", max_length=200)
     content= models.TextField(verbose_name="Contenido")
+    order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created= models.DateField(auto_now_add=True, verbose_name="fecha de Creacion")
     updated= models.DateField(auto_now=True, verbose_name="Modificado")
     
     class Meta:
         verbose_name = "Pagina"
         verbose_name_plural = "Paginas"
-        ordering = ['title']
+        ordering = ['order','title']
         
     def __str__(self):
         return self.title
